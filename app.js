3d543c30186c68557ffee29e70a0c51f14000746
jaykumar-bhatt1025 <jaykumar.bhatt1025@gmail.com>
2024-03-02 20:43:49 +0530

    add: changes

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ const cors = require('cors')
 const app = express();
 
 app.use(cors())
-app.use('/api',require('./apis'))
+// app.use('/api',require('./apis'))
+app.get("/api/users", (req,res)=> {
+  console.log('users')
+  return res.send('hello users')
+})
+app.get("/api/products", (req,res)=> {
+  console.log('products')
+  return res.send('hello products')
+})
 
 module.exports = app;
